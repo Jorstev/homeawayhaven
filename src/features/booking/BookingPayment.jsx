fdb3f89e3c819b5df5cfef7f455cbd615a9ace56
Jorstev <71983628+Jorstev@users.noreply.github.com>
2024-09-15 19:50:41 -0600

    Calendar validation added, working on responsive styles and refactoring code.

--- src/features/booking/BookingPayment.jsx
+++ src/features/booking/BookingPayment.jsx
@@ -34,6 +34,7 @@ function BookingPayment() {
     watch,
     formState: { errors },
     setValue,
+    trigger,
   } = useForm();
 
   const [dates, setDates] = useState(null);
@@ -45,19 +46,19 @@ function BookingPayment() {
 
   const onSubmit = (data) => console.log(data);
 
-  console.log(watch("Calendar"));
-  console.log(dates);
+  console.log(watch("dates"));
+  //   console.log(dates);
 
   return (
     <div>
       <form
-        className="flex flex-col w-full items-center space-y-6"
+        className="flex flex-col w-full items-center space-y-6 py-10"
         onSubmit={handleSubmit(onSubmit)}
       >
-        <label className="flex flex-col font-light">
+        <label className="flex flex-col md:flex-row md:items-center font-light">
           Reservation Place
           <input
-            className=" bg-gray-100 rounded-lg px-8 py-2"
+            className=" bg-gray-100 rounded-lg px-8 py-2 md:ml-5"
             type="text"
             placeholder="Hotel Name here"
             value={title}
@@ -122,51 +123,67 @@ function BookingPayment() {
         <label className="flex flex-col card justify-content-center font-light">
           Check In - Check Out Dates
           <Calendar
-            {...register("Calendar", {
-              required: true,
-            })}
             inputStyle={{
               width: "250px",
               backgroundColor: "#f3f4f6",
               padding: "0.5rem 2rem",
             }}
-            value={dates}
-            onChange={(e) => setDates(e.value)}
             selectionMode="range"
-            readOnlyInput
             hideOnRangeSelection
+            readOnlyInput
             placeholder="09/02/2024 - 09/06/2024"
             minDate={new Date()}
+            {...register("Calendar", { required: true })}
+            value={dates}
+            onChange={(e) => {
+              setDates(e.value);
+            }}
           />
         </label>
+
         {errors?.Calendar?.type === "required" && (
           <p className="text-red-500">⚠ This field is required</p>
         )}
-        {errors?.Calendar?.type === "pattern" && (
-          <p className="text-red-500">⚠ Incorrect Calendar Input</p>
-        )}
+
         <label className="flex flex-col font-light">
           Card Number
           <input
             className=" bg-gray-100 rounded-lg px-8 py-2"
             type="text"
-            placeholder="1234 1234 1234 1234"
-            {...register("Card Number", { required: true })}
+            placeholder="1234123412341234"
+            {...register("CardNumber", {
+              required: true,
+              minLength: 16,
+            })}
           />
         </label>
+
+        {errors?.CardNumber?.type === "minLength" && (
+          <p className="text-red-500">⚠ Card Number must be 16 numbers long</p>
+        )}
+        {errors?.CardNumber?.type === "required" && (
+          <p className="text-red-500">⚠ This field is required</p>
+        )}
         <div className="flex justify-center space-x-5 font-light">
           <label className="flex flex-col  max-w-[50%]">
             Expiration Date
             <input
-              className=" bg-gray-100 py-2 rounded-lg"
+              className={`bg-gray-100 rounded-lg py-2 ${
+                errors?.ExpirationDate?.type === "required"
+                  ? "border border-red-500"
+                  : ""
+              }`}
               type="date"
-              {...register("Expiration Date", { required: true })}
+              {...register("ExpirationDate", { required: true })}
             />
           </label>
-          <label className="flex flex-col max-w-[25%] font-light">
+
+          <label className="flex flex-col max-w-[25%] font-light ">
             CVV
             <input
-              className=" bg-gray-100 rounded-lg py-2"
+              className={`bg-gray-100 rounded-lg py-2 ${
+                errors?.CVV?.type === "required" ? "border border-red-500" : ""
+              }`}
               type="password"
               placeholder="555"
               {...register("CVV", { required: true })}
@@ -174,7 +191,7 @@ function BookingPayment() {
           </label>
         </div>
         <input
-          className="px-24 py-2 bg-cyan-300 rounded-lg text-white"
+          className="px-24 py-2 bg-cyan-300 rounded-lg text-white cursor-pointer"
           type="submit"
         />
       </form>
